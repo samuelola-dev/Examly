3713c40e46e94a55389fc2e1ba2973c2eadd665a
Samuel Ola <samuelola396@gmail.com>
2026-03-18 23:49:22 +0100

    feat: register and logging user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,23 +1,41 @@
+import bcrypt from "bcryptjs";
+
 import { User } from "../models/user.model.js";
+
 import { sendSuccess } from "../utils/response.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/error.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 // Registration Logic
 export const register = asyncHandler(async(req, res) => {
     const {name, matric_number, password, role} = req.body;
 
-    const existing = await User.findOne({ where: { matric_number } });
-    if (existing) throw ApiError.badRequest("User already exists");
+    // Check if ur exists
+    const user = await User.findOne({ where: { matric_number } });
+    if (user) throw ApiError.badRequest("User already exists");
     
-    const user = await User.create({name, matric_number, password, role});
+    // Create User
+    await User.create({name, matric_number, password, role});
+    
+    // Send User
     sendSuccess(res, {name, matric_number}, "User Registered Successfully") ; 
 });
 
-export const login = (req, res) => {
-    res.send("Login")
-};
+export const login = asyncHandler(async(req, res) => {
+    const {matric_number, password} = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({ where: { matric_number } });
+    if (!user) throw ApiError.badRequest("Invalid matric number or password");
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw ApiError.badRequest("Invalid matric number or password");
+
+    // Send response
+    sendSuccess(res, {name: user.name, role: user.role}, "Login Successful");
+});
 
 export const showRegister = (req, res) => {
-    res.send("What's sup");
+    
 };
